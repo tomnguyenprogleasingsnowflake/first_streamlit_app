0090da4b6190324f1091fda9a626a7553893bb9e
tomnguyenprogleasingsnowflake <128190974+tomnguyenprogleasingsnowflake@users.noreply.github.com>
2023-03-17 16:54:02 -0600

    Update streamlit_app.py
    spark didnt work using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
-my_fruit_list = spark.read.csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+from pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 stream.dataframe(my_fruit_list)
