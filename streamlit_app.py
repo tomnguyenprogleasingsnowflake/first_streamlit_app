2a5d004d1b6272e30c11fb3d835fe662c84d0ef9
tomnguyenprogleasingsnowflake <128190974+tomnguyenprogleasingsnowflake@users.noreply.github.com>
2023-03-17 16:47:05 -0600

    Update streamlit_app.py
    added title, header,text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-print('Hello world!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebery Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
