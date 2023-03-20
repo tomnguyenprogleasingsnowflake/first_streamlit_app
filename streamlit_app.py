0c2a434d44a9badbc509e29cec42d4c003329cdc
tomnguyenprogleasingsnowflake <128190974+tomnguyenprogleasingsnowflake@users.noreply.github.com>
2023-03-20 17:00:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruit_to_show)
+
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
