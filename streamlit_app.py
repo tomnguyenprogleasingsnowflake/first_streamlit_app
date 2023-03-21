e1b4af6036e597ccf406ce24b4ea4b4438d4d234
tomnguyenprogleasingsnowflake <128190974+tomnguyenprogleasingsnowflake@users.noreply.github.com>
2023-03-21 16:52:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,7 @@ streamlit.header("The fruit load list contains:")
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.dataframe(my_data_row)
+
+# input from user
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write("Thanks for adding, ", add_my_fruit)
